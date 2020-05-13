9ffe3b553a649e27274b2cd08d8e803a10483167
Andrew A. Stenipanin <and5251@yandex.ru>
2020-05-13 22:10:18 +0300

    add weather icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import WeatherCard from './components/WeatherCard/component';
+import WeatherCard from './components/WeatherCard/WeatherCard';
 
 function App() {
   return (
     <div className="App">
-      <WeatherCard />
+      <WeatherCard temp={-20} condition="Clear"/>
+      <WeatherCard temp={20} condition="Clouds"/>
+      <WeatherCard temp={40} condition="Dust"/>
     </div>
   );
 }
