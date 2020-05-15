d425c786e20c6c9a3e6d6f9f7a69f031c64e31b3
Andrew A. Stenipanin <and5251@yandex.ru>
2020-05-15 12:23:13 +0300

    add loading, error

---
+++ src/components/WeatherEngine.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import WeatherCard from "./WeatherCard/WeatherCard";
+
+const WeatherEngine = ({ location }) => {
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  });
+
+  const getWeather = async (q) => {
+    setQuery("")
+    setLoading(true);
+    try {
+      const apiRes = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&&APPID=3c5c33d4640921eb2db5d4cfeba9cd1e`
+      );
+      const resJSON = await apiRes.json();
+      setWeather({
+        temp: resJSON.main.temp,
+        city: resJSON.name,
+        condition: resJSON.weather[0].main,
+        country: resJSON.sys.country,
+      });
+    } catch (error) {
+      setError(true);
+    }
+
+    setLoading(false);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getWeather(query);
+  };
+
+  useEffect(() => {
+    getWeather(location);
+  }, [location]);
+
+  return (
+    <div className="App">
+      {!loading && !error ? (
+        <div>
+          <WeatherCard
+            temp={weather.temp}
+            condition={weather.condition}
+            city={weather.city}
+            country={weather.country}
+          />
+          <form>
+            <input value={query} onChange={(e) => setQuery(e.target.value)} />
+            <button onClick={(e) => handleSearch(e)}>Search</button>
+          </form>
+        </div>
+      ) : loading ? (
+        <div style={{ color: "black" }}>Loading</div>
+      ) : !loading && error ? (
+        <div style={{ color: "black" }}>
+          There has been an error!
+          <button onClick={() => setError(false)}>Reset!</button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default WeatherEngine;
